1b92c7799f7b8cdb407ca5688031f20b51f3cb79
ZHS <zhs10011000@163.com>
2024-10-06 13:27:47 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 from markupsafe import escape
+name = 'ZHS'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
+@app.route('/hello')
 def hello():
     return 'hello'
 
